9a30a7ec71f5d214947cb1a758f638bf61f2fd91
khan <rizwan88@.com>
2020-09-13 17:42:52 +0530

    Check Prime Single no

--- prime.cpp
+++ prime.cpp
@@ -11,7 +11,7 @@ int main()
     {
         if(n%i==0)
         {
-            cout<<n<<"  Is Not Prime";
+            cout<<n<<"  Is Not Prime ";
             flag=1;
             break;
         }
