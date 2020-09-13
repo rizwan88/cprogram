6c862b9e0ee8d152d7f5325a7c9561b2501e4b27
khan <rizwan88@.com>
2020-09-13 17:39:12 +0530

    PChack Prime in single no

---
+++ prime.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+//Check no is prime or not prime
+using namespace std;
+int main()
+{
+    int n,m,i,flag=0;
+    cout<<"\n enter a no";
+    cin>>n;
+    m=n/2;
+    for(i=2;i<=m;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<n<<"  Is Not Prime";
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        cout<<n<<"  Is Prime at Possition = "<<i;
+        return 0;
+    }
+}
